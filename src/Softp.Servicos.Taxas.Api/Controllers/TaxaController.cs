72e19ed7db1d21508bd7cee803c003c84364c711
Jeferson Michelinm <jeumichelin@gmail.com>
2020-12-02 00:26:49 -0300

    Versão Inicial API Gerenciamento de Taxas

---
+++ src/Softp.Servicos.Taxas.Api/Controllers/TaxaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Softp.Servicos.Taxas.Infra.CrossCutting.Constants;
+
+namespace Softp.Servicos.Taxas.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaController : ControllerBase
+    {
+        /// <summary>
+        /// Obter a taxa de juros padrão
+        /// </summary>
+        [HttpGet("/taxaJuros")]
+        public double ObterTaxaDeJuros()
+        {
+            return CTaxaPadrao.Juros;
+        }
+    }
+}
---
+++ src/Softp.Servicos.Taxas.Infra.CrossCutting/Constants/CTaxaPadrao.cs
@@ -0,0 +1,7 @@
+﻿namespace Softp.Servicos.Taxas.Infra.CrossCutting.Constants
+{
+    public static class CTaxaPadrao
+    {
+        public const double Juros = 0.01;
+    }
+}
